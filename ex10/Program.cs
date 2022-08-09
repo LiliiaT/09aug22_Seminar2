7ff6f12c1a0623991f100bb651376957267b9d9d
Liliia <liliia.titova200699@gmail.com>
2022-08-10 00:51:14 +0300

    ex10_less2

---
+++ ex10/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задание 10
+
+Console.WriteLine("Введите трехзначное число:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber >= 100 && firstNumber <= 999)
+{
+    int SecondDigit (int number)
+{
+    int result = (firstNumber / 10) % 10;
+
+    return result;
+}
+Console.WriteLine( SecondDigit (firstNumber));
+}
+
+else 
+{
+    Console.WriteLine("это не трехзначное число:");
+}
