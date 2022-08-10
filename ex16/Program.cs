836033c1b60499638a66108c893ba21a4578a64c
Liliia <liliia.titova200699@gmail.com>
2022-08-10 13:49:16 +0300

    ex16_less2

---
+++ ex16/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задание 16
+Console.WriteLine("Введите число от 1 до 7");
+int Number = Convert.ToInt32(Console.ReadLine());
+if  ((Number == 1) || (Number == 2) || (Number == 3) || (Number == 4) || (Number == 5 ))
+{
+    Console.WriteLine("нет");
+}
+else
+{
+    if ((Number == 6) || (Number == 7))
+      {
+        Console.WriteLine("да");
+      }
+      else
+      {
+        Console.WriteLine("нет такого дня недели");
+      }
+}
+
+    
