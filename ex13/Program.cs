1e6ea9548f9a4221b651b99aa56554e57a297725
Liliia <liliia.titova200699@gmail.com>
2022-08-10 13:48:41 +0300

    ex13_less2

---
+++ ex13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 13
+Console.Clear();
+Console.WriteLine("Введите число:");
+int Number = Convert.ToInt32(Console.ReadLine());
+string num = Number.ToString();
+if ( Number >= 100 )
+{
+    Console.WriteLine( num[2] );
+}
+else 
+{
+    Console.WriteLine ("третьей цифры нет");
+}
