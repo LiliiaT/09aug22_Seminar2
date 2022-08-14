0224c680bf81e0315116503c5db39b0e4daea438
Liliia <liliia.titova200699@gmail.com>
2022-08-14 13:44:09 +0300

    ex19_less3

---
+++ ex19/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задание 19
+int number = ReadInt("Введите пятизначное число: ");
+string num = number.ToString();
+
+ if ((num [0]== num[4]) && (num [1] == num[3]))
+    {
+        Console.WriteLine("число палиндром");
+       
+    }
+else 
+    {
+        Console.WriteLine("число не палиндром");
+    
+    }
+
+//*************************
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
