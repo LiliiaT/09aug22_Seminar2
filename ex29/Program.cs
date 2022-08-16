4e6171c62a0d4e0ca0536fd14646a2896df2e486
Liliia <liliia.titova200699@gmail.com>
2022-08-16 23:36:49 +0300

    ex29_hometask

---
+++ ex29/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 29. Задает массив из N элементов и выводит на экран отдельным методом
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[number];
+FillArray(numbers);
+PrintArray(numbers);
+
+//********************
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 101);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
