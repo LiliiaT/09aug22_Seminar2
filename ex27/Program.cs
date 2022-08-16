4f0202b4ffbf113c422caaf0363a47dfac3cd77b
Liliia <liliia.titova200699@gmail.com>
2022-08-16 23:35:59 +0300

    ex27_hometask

---
+++ ex27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27 принимает на вход число и выдает сумму цифр в числе - 452 - 11
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+//*************************
+int DigitsSum (int number)
+{
+    int sum = 0;
+    while( number > 0)
+    {
+        sum += number %10;
+    number /=10;
+    }
+
+return sum;
+}
+Console.WriteLine (DigitsSum (number));
+
+
+
+
+
+
+
+
+
