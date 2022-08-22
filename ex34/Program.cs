1d64800e7ae21b523072964ba7e2a19db0e3a9c3
Liliia <liliia.titova200699@gmail.com>
2022-08-22 13:31:01 +0300

    ex34_hometask

---
+++ ex34/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 34
+int size = 10;
+int[] numbers = new int[size] ;
+int count = 0;
+FillArrayRandomNumbres(numbers);
+PrintArray(numbers);
+
+for (int i = 0; i < numbers.Length; i++)
+
+{
+    if( numbers[i] %2 == 0)
+    {
+        count++;
+    }
+        
+}
+Console.WriteLine($"количество четных чисел в данном массиве: {count}");
+
+//***********************
+
+void FillArrayRandomNumbres(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
