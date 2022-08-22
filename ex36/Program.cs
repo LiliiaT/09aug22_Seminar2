e673b7d71761b0c8fd89689c2d79a0f17111b49d
Liliia <liliia.titova200699@gmail.com>
2022-08-22 13:31:56 +0300

    ex36_hometask

---
+++ ex36/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 36
+
+int size = 10;
+int[] numbers = new int[size];
+int count = 0;
+FillArrayRandomNumbres(numbers);
+PrintArray(numbers);
+int sum = 0;
+
+
+for (int i = 1; i < numbers.Length; i=i+2)
+{
+    sum += numbers[i];
+    count++;
+}
+Console.WriteLine(sum);
+
+
+void FillArrayRandomNumbres(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 11);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write( "[{0}]", string.Join (",", array[i] ));
+    }
+    Console.WriteLine();
+}
+
