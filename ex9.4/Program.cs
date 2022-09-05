6682b0e516b019227fd891decb98bf0646eaa5bd
Liliia <liliia.titova200699@gmail.com>
2022-09-05 20:18:27 +0300

    ex9.4_hometask

---
+++ ex9.4/Program.cs
@@ -0,0 +1,18 @@
+﻿// 9.4 Написать программу вычисления функции Аккермана
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Ackermann = Ack(m, n);
+
+Console.Write($"Функция Аккермана = {Ackermann} ");
+
+int Ack(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return Ack(m - 1, 1);
+    else return Ack(m - 1, Ack(m, n - 1));
+}
+
