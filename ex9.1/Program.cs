d16f1dcde4c3efbeba79dc10314de90108185165
Liliia <liliia.titova200699@gmail.com>
2022-09-05 20:16:43 +0300

    ex9.1_hometask

---
+++ ex9.1/Program.cs
@@ -0,0 +1,16 @@
+﻿// 9.1 Показать натуральные числа от n до 1, n задано
+
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = 1;
+
+string numbersRec(int n, int m)
+{
+    if (n <= m) return numbersRec(n + 1, m) + $"{n} ";
+    else return String.Empty;
+}
+
+Console.WriteLine(numbersRec(m,n)); 
+
+
