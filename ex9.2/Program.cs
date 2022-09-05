4376a534245d203300fef977338da522c6f61c8e
Liliia <liliia.titova200699@gmail.com>
2022-09-05 20:17:28 +0300

    ex9.2_hometask

---
+++ ex9.2/Program.cs
@@ -0,0 +1,13 @@
+﻿//9.2 Показать натуральные числа от M до N, N и M заданы
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string NumbersRec(int m, int n)
+{
+    if (m <= n) return NumbersRec(m + 1, n) + $"{m} ";
+    else return String.Empty;
+}
+
+Console.WriteLine(NumbersRec(n,m)); 
